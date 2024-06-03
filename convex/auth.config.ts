744cb9b4b0125272f0d19403b22f7c6ee31e8db3
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2024-06-03 00:30:42 -0600

    v1

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,7 @@
 const authConfig = {
   providers: [
     {
-      domain: "https://fond-sawfly-54.clerk.accounts.dev",
+      domain: "https://holy-oryx-62.clerk.accounts.dev",
       applicationID: "convex",
     },
   ]
