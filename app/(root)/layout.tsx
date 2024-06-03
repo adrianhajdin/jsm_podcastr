1fc4de881c24cd6d6879fb9d4783df8591965c5d
SpreeGore <89117156+SpreeGore@users.noreply.github.com>
2024-06-03 00:08:26 -0600

    V1

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,7 +4,7 @@ import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster"
 import PodcastPlayer from "@/components/PodcastPlayer";
-import ConvexClientProvider from "../providers/ConvexProvider";
+
 
 export default function RootLayout({
   children,
